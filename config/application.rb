a04d4c5c672b21415a7efd40b98ed92e5f1b1a19
Cody Anderson <codyanderson@unomaha.edu>
2017-07-11 07:56:28 +0000

    Deploy to Heroku?

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Workspace
   class Application < Rails::Application
     config.web_console.whiny_requests = false
+    config.web_console.development_only = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
