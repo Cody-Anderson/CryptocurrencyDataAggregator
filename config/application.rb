f55f170fd3a0fcfa959385d457c9072aeb60615d
Jason Stratman <jpstratman@unomaha.edu>
2017-08-04 14:14:23 +0000

    Disabled Poloniex

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module Workspace
           
           loop do
             
-            pullPoloniex(timeToPull, pairsToPull)
+            # pullPoloniex(timeToPull, pairsToPull)
             pullBittrex(timeToPull, pairsToPull)
             
             # Set time to pull to slightly larger than two intervals
