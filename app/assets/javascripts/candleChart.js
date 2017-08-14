97602fdabb4573f32286853af8e142e85d4998be
Brock Hinkson <brockhinkson@gmail.com>
2017-08-14 04:44:01 -0500

    Added multiple series display functionality

--- app/assets/javascripts/candleChart.js
+++ app/assets/javascripts/candleChart.js
@@ -2,11 +2,20 @@ $(document).on('turbolinks:load', function() {
 
     var arr = $('#candles').data('temp');       // Import chart data
     var ex = $('#exchange').data('temp');       // Import exchange
-    var p1 = $('#pair1').data('temp');
+    var p1 = $('#pair1').data('temp');          // Import pair data
     var p2 = $('#pair2').data('temp');
+    var all = $('#all').data('temp');           // If true, show all exchange data
+    var minV = $('#min').data('temp');          // Minimum vertical axis value
+    var maxV = $('#max').data('temp');          // Maximum vertical axis value
     
     var pair = p1 + "/" + p2;           // Form chart title from passed-in
     var exchange = pair + " - " + ex;     // exchange and pair
+    var leg = "none";
+    
+    if( all == true ) {         // Removes the exchange from the title, if all exchanges are wanted
+        exchange = pair;        // Also adds a legend if there are multiple series
+        leg = "bottom"
+    }
 
     for( var i = 0; i < arr.length; i++ ) {     // Convert each passed-in time string to
         arr[i][0] = new Date(arr[i][0]);        // a date object
@@ -16,12 +25,12 @@ $(document).on('turbolinks:load', function() {
     google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
-        var data = google.visualization.arrayToDataTable(arr, true);
+        var data = google.visualization.arrayToDataTable(arr, false);
 
         var options = {
             title: exchange,
             titleTextStyle: { fontSize: 30 },
-            legend: 'none',
+            legend: leg,
             chartArea: { width: '85%', height: '86%' },
             
             backgroundColor: { strokeWidth: 0 },
@@ -46,7 +55,9 @@ $(document).on('turbolinks:load', function() {
                         minutes: { format: ['HH:mm a Z', ':mm'] }
                     }
                 }
-            }
+            },
+            
+            vAxis: { viewWindow: { min: minV * 0.999, max: maxV * 1.001 } }
         };
 
         var chart = new google.visualization.CandlestickChart(document.getElementById('chart_div'));
--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -15,6 +15,12 @@ class ExchangesController < ApplicationController
       @frame = eval(params[:tf])
     end
     
+    if( params[:all] == 'true' )     # Sets @all variable to be true if all exhanges are wanted
+      @all = true
+    elsif( params[:all] == 'false' )   # Sets @all variable to be false if one exchange is wanted
+      @all = false
+    end
+    
     @pair1 = pair[0..2]             # First part of pair
     @pair2 = pair[4..-1]            # Second part of pair
     @pair = "#{@pair1}_#{@pair2}"   # Entire pair
@@ -32,8 +38,81 @@ class ExchangesController < ApplicationController
         interval = 86400    # For a month-long timeframe, use 24 hour candles
     end
     
-    # Assigns the appropriate data to @candles, according the the exchange, pair, and interval data
-    @candles = Candlestick.order( :timestamp ).where( :timestamp => (Time.now - @frame)..(Time.now),
+    # Minimum and maximum verticle axis values will be calculated from these values
+    @min = Candlestick.where( :timestamp => (Time.now - @frame)..(Time.now), :exchange => @exchange, :pair => pair, :interval => interval ).minimum(:low)
+    @max = Candlestick.where( :timestamp => (Time.now - @frame)..(Time.now), :exchange => @exchange, :pair => pair, :interval => interval ).maximum(:high)
+    
+    # Sets up the data variable based on if it wants one or multiple series
+    if( @all == false )
+      # Assigns the appropriate data to @candles, according the the exchange, pair, and interval data
+      @candles = Candlestick.order( :timestamp ).where( :timestamp => (Time.now - @frame)..(Time.now),
                 :exchange => @exchange, :pair => pair, :interval => interval ).pluck( :timestamp, :low, :open, :close, :high )
+      
+      # Sets up the label information for the legend
+      labels = [ '', @exchange, '', '', '' ]
+      @candles = @candles.unshift( labels )
+      
+    # Sets up the @candles array to display multiple series
+    else
+      @candles = Candlestick.order( :timestamp ).where( :timestamp => (Time.now - @frame)..(Time.now),
+                :exchange => "Bittrex", :pair => pair, :interval => interval ).pluck( :timestamp, :low, :open, :close, :high )
+      
+      @candles2 = Candlestick.order( :timestamp ).where( :timestamp => (Time.now - @frame)..(Time.now),
+                :exchange => "Poloniex", :pair => pair, :interval => interval ).pluck( :timestamp, :low, :open, :close, :high )
+      
+      # Accounts for the case where BTC_ZEC or BTC_SC pairs are shown (Bitfinex doesn't have these)
+      unless( :pair == 'BTC_ZEC' || :pair == 'BTC_SC' )
+      
+        # Calculates the new mins and maxes
+        @min = Candlestick.where( :timestamp => (Time.now - @frame)..(Time.now), :pair => pair, :interval => interval ).minimum(:low)
+        @max = Candlestick.where( :timestamp => (Time.now - @frame)..(Time.now), :pair => pair, :interval => interval ).maximum(:high)
+      
+        @candles3 = Candlestick.order( :timestamp ).where( :timestamp => (Time.now - @frame)..(Time.now),
+                  :exchange => "Bitfinex", :pair => pair, :interval => interval ).pluck( :timestamp, :low, :open, :close, :high )
+      end
+      
+      i = 0            
+      @candles.each do |candle|   # Combines the first and second series so the graph can render them
+        
+        # Ensures that empty spots in a series are taken care of
+        if( @candles2[i] != nil && candle[0] == @candles2[i][0] )
+            candle.push( @candles2[i][1], @candles2[i][2], @candles2[i][3], @candles2[i][4] )
+            i = i + 1
+          else
+            candle.push( 0, 0, 0, 0 )
+            i = i + 1
+          end
+          
+      end
+      
+      # Combines the third series, if it's not BTC_ZEC or BTC_SC
+      unless( @pair == 'BTC_ZEC' || @pair == 'BTC_SC' )
+        i = 0
+        @candles.each do |candle|
+          
+          # Ensures that empty spots in a series are taken care of
+          if( @candles3[i] != nil && candle[0] == @candles3[i][0] )
+            candle.push( @candles3[i][1], @candles3[i][2], @candles3[i][3], @candles3[i][4] )
+            i = i + 1
+          else
+            candle.push( 0, 0, 0, 0 )
+            i = i + 1
+          end
+          
+        end
+      end
+      
+      # This sets up the legend for the chart to interpret
+      if( @pair == 'BTC_ZEC' || @pair == 'BTC_SC' )
+        labels = [ '', 'Bittrex', '', '', '', 'Poloniex', '', '', '' ]
+      else
+        labels = [ '', 'Bittrex', '', '', '', 'Poloniex', '', '', '', 'Bitfinex', '', '', '' ]
+      end
+      
+      # Puts the labels array at the beggining of @candles, and shifts the rest forward one index
+      @candles = @candles.unshift( labels )
+      
+    end
+  
   end
 end
